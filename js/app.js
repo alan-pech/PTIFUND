ca2feb1464611b4f75527b95e79f214893c64ee6
alan-pech <alan@pech.co.uk>
2026-01-03 20:07:59 +0000

    Arrange attached assets to right of slide 3

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
  */
 
 // --- Constants & State ---
-const APP_VERSION = 'v1.0.070';
+const APP_VERSION = 'v1.0.071';
 const ADMIN_ROUTE_SECRET = 'admin-portal'; // Accessible via index.html#admin-portal
 
 let currentUser = null;
