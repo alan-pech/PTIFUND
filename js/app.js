e9ca78ad9ed963e348f19c2c6054d9f8c221f9e7
alan-pech <alan@pech.co.uk>
2026-01-04 12:53:39 +0000

    Fix overlapping gallery on mobile

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
  */
 
 // --- Constants & State ---
-const APP_VERSION = 'v1.0.079';
+const APP_VERSION = 'v1.0.080';
 const ADMIN_ROUTE_SECRET = 'admin-portal'; // Accessible via index.html#admin-portal
 
 let currentUser = null;
